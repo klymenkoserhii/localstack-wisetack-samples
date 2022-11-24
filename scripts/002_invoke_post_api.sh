49158d512582182dafa97056cdd505c403c9e5ed
Serhii Klymenko <serhii@wisetack.com>
2022-11-24 08:34:32 +0200

    Simplify API spec

---
+++ scripts/002_invoke_post_api.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+
+source ./000_get_api_id.sh
+start=`date +%s`
+
+data=`curl -v -H "Content-Type: application/json" -H "User-Agent: unirest-java/curl" \
+-d "{\"test\": \"test\"}" -X POST ${API_URL}/products/abcd-1234/items?status=PENDING\&limit=100`
+
+echo ${data} | jq
+
+end=`date +%s`
+echo "Execution time was `expr $end - $start` seconds."
--- scripts/003_invoke_get_api.sh
+++ scripts/003_invoke_get_api.sh
@@ -2,12 +2,11 @@
 
 set -e
 
-
 source ./000_get_api_id.sh
 start=`date +%s`
 
 data=`curl -v -H "Content-Type: application/json" -H "User-Agent: unirest-java/curl" \
--d "{\"test\": \"test\"}" -X POST ${API_URL}/merchants/abcd-1234/transactions?status=PENDING\&limit=100`
+${API_URL}/products/abcd-1234/items?status=PENDING\&limit=100`
 
 echo ${data} | jq
 
